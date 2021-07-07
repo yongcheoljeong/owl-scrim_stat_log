fc90374585864d5f61b28e18efba9977e0b788bb
jyc1306 <jyc1306@gmail.com>
2021-07-07 21:09:23 +0900

    add MapNameList

---
+++ MapNameList.py
@@ -0,0 +1,34 @@
+mapnamelist = [
+    "Dorado",
+    "King's Row", 
+    "Kings' Row",
+    "Temple of Anubis",
+    "Anubis",
+    "Busan",
+    "Eichenwalde",
+    "Hanamura",
+    "Blizzard World",
+    "Havana",
+    "Nepal",
+    "Volskaya Industries",
+    "Volskaya",
+    "Oasis",
+    "Watchpoint: Gibraltar",
+    "Watchpoint Gibraltar",
+    "Ilios",
+    "Lijiang Tower",
+    "Lijang Tower",
+    "Numbani",
+    "Junkertown",
+    "Hollywood",
+    "Rialto",
+    "Route 66",
+]
+
+mapname_typocorrection = {
+    "Kings' Row":"King's Row",
+    "Anubis":"Temple of Anubis",
+    "Volskaya":"Volskaya Industries",
+    "Watchpoint Gibraltar":"Watchpoint: Gibraltar",
+    "Lijang Tower":"Lijiang Tower"
+}
